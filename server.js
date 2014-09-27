e998eb7a81829c0782c1c619ac1a82bfe65815db
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-09-27 22:34:51 +0300

    Update for sending email

--- server.js
+++ server.js
@@ -8,20 +8,6 @@ var express = require('express'),
 
 var emailService = new EmailService('./config.json');
 
-emailService.sendMail({
-	to : 'Kirill.Parfenkov@gmail.com',
-	subject : "Server started",
-	text : "server started",
-	html : "<h1>Server started!</h1>"
-}, function( err, info ) {
-	if ( err ){
-		console.log( err );	
-	} else {
-		console.log( 'Email service work!' );
-		console.log( info );
-	}
-});
-
 var app = express();
 
 
@@ -32,7 +18,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(serveStatic('public'));
 
+app.post( '/api/services/email', function(req, res) {
+    var message = req.body.message;
+    var sander = req.body.sender;
+    emailService.sendMail({
+        to : 'Kiryl.Parfiankou.Dev@gmail.com',
+        subject : "Message from the parf.by",
+        text : message,
+        html : "<p>From: " + sander+ "</p><p>" + message + "</p>"
+    }, function( err ) {
+        if ( err ) {
+            console.log('err');
+            console.log( err );
+            res.json( 400, err);
+        } else {
+            res.json( 200, {});
+        }
+    });
+});
 
-app.listen(80, function() {
-	console.log('Server running at 80 port');
+app.listen(8080, function() {
+	console.log('Server running at 8080 port');
 });
